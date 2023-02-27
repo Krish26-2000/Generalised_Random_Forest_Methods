cf98734dda02d7dc43294376dede28782df64e4c
Krish26-2000 <krishnaakolkar16@gmail.com>
2023-02-27 09:03:39 +0100

    jupyter notebook and changed the saving dataset function

--- src/generalised_random_forest_methods/data_management/task_data_management.py
+++ src/generalised_random_forest_methods/data_management/task_data_management.py
@@ -1,16 +1,16 @@
 import pytask
 from generalised_random_forest_methods.config import BLD, SRC
 from generalised_random_forest_methods.data_management.clean_data import (
-    ipums_2019_data, ipums_2015_data,
+    ipums_data, 
 )
 
-url_1 = (
-    "https://www.dropbox.com/s/0gsml2tasys0deu/usa_00004.csv?dl=1"
+url = (
+    "https://www.dropbox.com/s/0gsml2tasys0deu/usa_00006.csv?dl=1"
 )
 
 #@pytask.mark.depends_on(url)
 @pytask.mark.produces(BLD / "python" / "usa_00004.csv")
-def task_save_ipums__2019_data(produces):
+def task_save_ipums_data(produces):
     """Import IPUMS data from Dropbox folder and save in the BLD folder
 
     Args: 
@@ -20,25 +20,5 @@ def task_save_ipums__2019_data(produces):
         The IPUMS dataset
     
     """
-    data_2019 = ipums_2019_data(url_1)
+    data_2019 = ipums_data(url)
     data_2019.to_csv(produces)
-
-url_2 = (
-    "https://www.dropbox.com/s/0gsml2tasys0deu/usa_00005.csv?dl=1"
-)
-
-#@pytask.mark.depends_on(url)
-@pytask.mark.produces(BLD / "python" / "usa_00005.csv")
-def task_save_ipums__2015_data(produces):
-    """Import IPUMS data from Dropbox folder and save in the BLD folder
-
-    Args: 
-        produces: Specify the folder path to locate the file
-
-    Returns:
-        The IPUMS dataset
-    
-    """
-    data_2015 = ipums_2015_data(url_2)
-    data_2015.to_csv(produces)
-
