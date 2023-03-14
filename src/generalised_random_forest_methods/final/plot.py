0d281dab035cc0a53458d5604a93637dd6615272
cheekoti_la <abhilash.cheekoti@mpinb.mpg.de>
2023-03-14 21:57:06 +0100

    wrote functions for 2nd hypothesis and tasks

--- src/generalised_random_forest_methods/final/plot.py
+++ src/generalised_random_forest_methods/final/plot.py
@@ -5,6 +5,7 @@ import pandas as pd
 from generalised_random_forest_methods.config import BLD
 
 path = BLD / "python" / "data" / "z_rolling_means.pkl"
+path_2 = BLD / "python" / "data" / "z_rolling_means2.pkl"
 
 
 def treatment_effect_plot(z):
@@ -23,3 +24,19 @@ def treatment_effect_plot(z):
     ax.legend()
     return fig
 
+
+def treatment_effect_plot2(z2):
+    z2 = pd.read_pickle(path_2)
+    fig2, ax = plt.subplots(figsize =(12, 8))
+    # plotlines for treatment effects and confidence intervals
+    ax.plot(z2['cate'],
+            marker='.', linestyle='-', linewidth=0.5, label='CATE', color='indigo')
+    ax.plot(z2['lb'],
+            marker='.', linestyle='-', linewidth=0.5, color='steelblue')
+    ax.plot(z2['ub'],
+            marker='.', linestyle='-', linewidth=0.5, color='steelblue')
+    # axes and create legend
+    ax.set_ylabel('Treatment Effects')
+    ax.set_xlabel('Number of observations')
+    ax.legend()
+    return fig2
--- src/generalised_random_forest_methods/final/task_final.py
+++ src/generalised_random_forest_methods/final/task_final.py
@@ -4,7 +4,7 @@ from generalised_random_forest_methods.config import GROUPS
 from generalised_random_forest_methods.config import SRC
 
 import pandas as pd
-from generalised_random_forest_methods.final.plot import treatment_effect_plot
+from generalised_random_forest_methods.final.plot import treatment_effect_plot, treatment_effect_plot2
 
 
 @pytask.mark.depends_on(BLD / "python" / "data" / "z_rolling_means.pkl")
@@ -12,5 +12,13 @@ from generalised_random_forest_methods.final.plot import treatment_effect_plot
 def task_plot_treatment_effects(depends_on, produces):
     z = pd.read_pickle(depends_on)
     plot = treatment_effect_plot(z)
+    plot.savefig(produces)
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "z_rolling_means2.pkl")
+@pytask.mark.produces(BLD / "python" / "plots" / "children_treatment_effects_plot.png")
+def task_plot_treatment_effects(depends_on, produces):
+    z2 = pd.read_pickle(depends_on)
+    plot = treatment_effect_plot2(z2)
     plot.savefig(produces)
 
