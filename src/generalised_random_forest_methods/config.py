4b5e4bbe5fac0d3469e07bb6b7c47fa332fa0ecf
cheekoti_la <abhilash.cheekoti@mpinb.mpg.de>
2023-03-25 14:47:25 +0100

    changes to latex

--- src/generalised_random_forest_methods/config.py
+++ src/generalised_random_forest_methods/config.py
@@ -8,24 +8,4 @@ BLD = SRC.joinpath("..", "..", "bld").resolve()
 TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
 PAPER_DIR = SRC.joinpath("..", "..", "paper").resolve()
 
-TASK_1 = [
-    "outcome_train",
-    "features_train",
-    "treatment_train",
-    "instrument_train",
-    "features_test",
-]
-
-TASK_2 = [
-    "outcome2_train",
-    "features2_train",
-    "treatment_train",
-    "instrument_train",
-    "features2_test",
-]
-
-GROUPS = ["marital_status", "qualification"]
-
-__all__ = ["BLD", "SRC", "TEST_DIR"]
-
-NO_LONG_TASKS = True
+__all__ = ["BLD", "SRC", "TEST_DIR", "PAPER_DIR"]
