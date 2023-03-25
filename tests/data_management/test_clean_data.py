56164534d7668cb06dfa742a55065cb1f88a0f0d
Krish26-2000 <krishnaakolkar16@gmail.com>
2023-03-25 16:01:48 +0100

    added test functions

--- tests/data_management/test_clean_data.py
+++ tests/data_management/test_clean_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from generalised_random_forest_methods.config import TEST_DIR
-from generalised_random_forest_methods.data_management import clean_data
+from generalised_random_forest_methods.data_management.clean_data import clean_data
 from generalised_random_forest_methods.utilities import read_yaml
 
 
@@ -23,30 +23,11 @@ def test_clean_data_drop_columns(data, data_info):
     assert not set(data_info["columns_to_drop"]).intersection(set(data_clean.columns))
 
 
-def test_clean_data_dropna(data, data_info):
-    data_clean = clean_data(data, data_info)
+def test_clean_data_dropna(data,data_info):
+    data_clean = clean_data(data,data_info)
     assert not data_clean.isnull().any(axis=None)
 
 
-def test_clean_data_categorical_columns(data, data_info):
-    data_clean = clean_data(data, data_info)
-    for cat_col in data_info["categorical_columns"]:
-        cat_col = data_info["column_rename_mapping"].get(cat_col, cat_col)
-        assert data_clean[cat_col].dtype == "category"
-
-
-def test_clean_data_column_rename(data, data_info):
-    data_clean = clean_data(data, data_info)
-    old_names = set(data_info["column_rename_mapping"].keys())
-    new_names = set(data_info["column_rename_mapping"].values())
-    assert not old_names.intersection(set(data_clean.columns))
-    assert new_names.intersection(set(data_clean.columns)) == new_names
-
-
-def test_convert_outcome_to_numerical(data, data_info):
-    data_clean = clean_data(data, data_info)
-    outcome_name = data_info["outcome"]
-    outcome_numerical_name = data_info["outcome_numerical"]
-    assert outcome_numerical_name in data_clean.columns
-    assert data_clean[outcome_name].dtype == "category"
-    assert data_clean[outcome_numerical_name].dtype == np.int8
+def test_married_year(data,data_info):
+    data_married_year_cleaned = clean_data(data,data_info)
+    assert data_married_year_cleaned["MARRINYR"].isin([2]).all()
